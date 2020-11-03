fa25e74fc091d5857f3fd7f1008708276ac0aabc
yuievon0ee <71679885+yuievon0ee@users.noreply.github.com>
2020-11-03 16:03:13 +0800

    按鈕程式和結束畫面

---
+++ unity2D20200922/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        print("開始遊戲");
+        SceneManager.LoadScene("關卡1");
+    }
+    public void QuitGame()
+    {
+        print("離開遊戲");
+        Application.Quit();
+    }
+  
+}
---
+++ unity2D20200922/Assets/Scripts/Player.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject final;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("碰到傳送門了");
+        final.SetActive(true);
+    }
+
+}
